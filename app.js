34ca1c06c1408bd97fefb17317807fcab74da2e3
DumitruPinzari <pinzari.dumitru@upsc.md>
2026-06-11 12:33:13 +0300

    Eveniment 6 mai 2026

--- app.js
+++ app.js
@@ -241,7 +241,39 @@ const studyPrograms = [
   }
 ];
 
-const evenimenteIstorice = [
+const evenimenteIstorice = [  {
+    date: '6 mai 2026',
+    title: 'Olimpiada Interuniversitară de Informatică – ediția a XV-a',
+    note: `Facultatea de Fizică, Matematică și Tehnologii Informaționale a Universității Pedagogice de Stat "Ion Creangă" din Chișinău a organizat cu succes "Olimpiada interuniversitară de informatică – ediția a XV-a", un eveniment academic dedicat studenților pasionați de informatică, programare și tehnologii informaționale.<br /><br />
+    
+    Evenimentul a fost organizat de către Catedra Informatică și Tehnologii Informaționale, una dintre subdiviziunile academice importante ale facultății, care contribuie activ la formarea specialiștilor în domeniul informaticii și al tehnologiilor digitale.<br /><br />
+    
+    <strong>Cadre didactice organizatoare:</strong><br />
+    • Liubomir Chiriac – doctor habilitat, profesor universitar, șef de catedră, membru corespondent al AȘM;<br />
+    • Andrei Braicov – profesor universitar, decan al facultății;<br />
+    • Angela Globa, Ala Gașnaș, Natalia Joșu, Maria Pavel, Teodora Vascan – doctori, conferențiari universitari;<br />
+    • Marina Bostan, Olesea Sîrghi – lectori universitari, doctori în științe ale educației.<br /><br />
+    
+    La competiție au participat și studenți de la Universitatea de Stat "Alecu Russo" din Bălți, însoțiți de prof. Olesea Skutnițki.<br /><br />
+    
+    <strong>Rezultate studenți UPSC:</strong><br />
+    <strong>Anul I:</strong><br />
+    - Goncear Vladimir – locul II;<br />
+    - Didic Victoria, Movilă Ștefan, Sandu Mihai – locul III;<br />
+    - Buză Maria-Magdalena – mențiune.<br /><br />
+    
+    <strong>Anul II–IV:</strong><br />
+    - Sandu Florin – locul I;<br />
+    - Cantea Aura, Sârbu Daniela, Păduraru Laurențiu – locul II;<br />
+    - Zalevschi Pavel, Josan Alexandru, Grecica Maxim, Zugravu Nicoleta, Pleșca Alexandrina – locul III;<br />
+    - Șipitca Sergiu, Poșcai Anastasia, Lungu Loredana, Benderscaia Iulia, Midari Denis, Gudumac Ecaterina – mențiune.`
+  },
+  {
+    date: '15 octombrie 1985',
+    title: 'Constituirea Catedrei Informatică și Tehnică de Calcul',
+    note: 'Prima catedră de informatică din Moldova, fondată în cadrul IPST/UST.'
+  },
+  
   {
     date: '15 octombrie 1985',
     title: 'Constituirea Catedrei Informatică și Tehnică de Calcul',
